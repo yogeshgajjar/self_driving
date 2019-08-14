d44082dbb886103b2082ebad829f7568ec822318
Yogesh Gajjar <ygajjar@usc.edu>
2019-08-14 13:46:59 -0700

    modified program

--- astar_algorithm.py
+++ astar_algorithm.py
@@ -1,7 +1,20 @@
+#########################################################################################################################################
+
+# This example illustrates the search algorithm using A* in a grid world and a goal location. 
+
+# OBJECTIVE - Write a program that finds the shortest path that leads the robot from the start state to the goal state and overcomes the problem faced by the traditional search algorithm.
+
+# GIVEN - 
+# A heuristic function of same dimensions as the main grid world to find a efficient shortest path to the goal state. The heurisitc function h(x,y) <= distance to goal from x,y
+# Five variables i.e. x,y,f,g & h where (x,y) are the grid dimensions, h is the value from the heuristic function at given (x,y), g is the shortest distance to the goal state & f is (g+h). 
+# Let us assume that the robot takes certain actions with certainity.
+
+###########################################################################################################################################
+
+
 # Grid format:
 #   0 = Navigable space
 #   1 = Occupied space
-
 
 
 
